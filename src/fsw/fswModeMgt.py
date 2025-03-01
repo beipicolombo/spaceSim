b4bdcefbf536ed686607bf7b2b7a473b9d4942bf
beipicolombo <beipicolombo@protonmail.com>
2025-03-01 22:02:51 +0100

    240 MOD add temporary AOCS modes NOM_PTNG with inertia attitude pointing and NOM_EQ with nadir pointing guidance, to be worked later

--- src/fsw/fswModeMgt.py
+++ src/fsw/fswModeMgt.py
@@ -14,7 +14,8 @@ class FswModeMgtParam:
 		self.aocsSafeModeMinDur = 0*60 # [s]
 		self.aocsSafeModeAngRateThd = 0.1*deg2rad # [s]
 		self.aocsSafeModeAngRateThdDur = 30*60 # [s]
-		self.aocsNomModeMinDur = 15*60 # [s]
+		self.aocsNomPtngModeMinDur = 15*60 # [s]
+		self.aocsNomEqModeMinDur = 90*60 # [s]
 		self.aocsNomModeAngRateThd = 0.01*deg2rad # [s]
 		self.aocsNomModeAngRateThdDur = 10*60 # [s]
 
@@ -62,10 +63,12 @@ class FswModeMgtState:
 			# Transition OFF -> SAFE
 			aocsMode = "SAFE"
 		elif ((self.aocsMode == "SAFE") and (self.aocsModeElapsedTime > modeMgtParam.aocsSafeModeMinDur) and isRateCvg):
-			# Transition SAFE -> NOM
-			aocsMode = "NOM"
-		elif ((self.aocsMode == "NOM") and (self.aocsModeElapsedTime > modeMgtParam.aocsNomModeMinDur) and isRateCvg):
+			# Transition SAFE -> NOM_PTNG
+			aocsMode = "NOM_PTNG"
+		elif ((self.aocsMode == "NOM_PTNG") and (self.aocsModeElapsedTime > modeMgtParam.aocsNomPtngModeMinDur) and isRateCvg):
 			# Transition NOM -> OCM, TBW dummy for now
+			aocsMode = "NOM_EQ"
+		elif ((self.aocsMode == "NOM_EQ") and (self.aocsModeElapsedTime > modeMgtParam.aocsNomEqModeMinDur) and isRateCvg):
 			aocsMode = "OCM"
 		else: 
 			# Otherwise in current mode
@@ -75,7 +78,7 @@ class FswModeMgtState:
 		if (aocsMode == "SAFE"):
 			angRateThd = modeMgtParam.aocsSafeModeAngRateThd
 			angRateThdDur = modeMgtParam.aocsSafeModeAngRateThdDur
-		elif (aocsMode == "NOM"):
+		elif ((aocsMode == "NOM_PTNG") or (aocsMode == "NOM_EQ")):
 			angRateThd = modeMgtParam.aocsNomModeAngRateThd
 			angRateThdDur = modeMgtParam.aocsNomModeAngRateThdDur
 		else:
@@ -133,8 +136,10 @@ def computeModeMgt(simParam, fswParam, fswModeMgtState, fswBus):
 def aocsGuidanceModeLogic(aocsMode):
 	if (aocsMode == "SAFE"):
 		aocsGuidMode = "GUIDMODE_RATE_DAMPING"
-	elif (aocsMode == "NOM"):
+	elif (aocsMode == "NOM_PTNG"):
 		aocsGuidMode = "GUIDMODE_ATT_INERT" 
+	elif (aocsMode == "NOM_EQ"):
+		aocsGuidMode = "GUIDMODE_ATT_NADIR" 
 	elif (aocsMode == "OCM"):
 		aocsGuidMode = "GUIDMODE_ATT_INERT"
 	else:
@@ -146,7 +151,7 @@ def aocsCtrModeLogic(aocsMode):
 	if (aocsMode == "SAFE"):
 		aocsCtrMode = "CTRLMODE_RATE_DAMP_CTRL"
 		aocsCtrActMode = "THR"
-	elif (aocsMode == "NOM"):
+	elif ((aocsMode == "NOM_PTNG") or (aocsMode == "NOM_EQ")):
 		aocsCtrMode = "CTRLMODE_ATT_CTRL"
 		aocsCtrActMode = "THR"
 	elif (aocsMode == "OCM"):

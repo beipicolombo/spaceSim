240 MOD to implement nadir pointing: add mode logic, add quaternion in all simulator, include LVLH computation to environment model, fix bugs / errors in attitude kinematics, orbit dynamics, update database

src/attitudeKinematics.py
@@ -25,16 +25,27 @@ class Quaternion:
     
     def norm(self):
         return np.linalg.norm(self.toVec())
+
+    def isIdentity(self):
+        vecnorm = np.linalg.norm(self.vec)
+        eps = 1e-10
+        isIdentity = ((vecnorm < eps) and ((self.sca-1)<eps))
+        return isIdentity
     
     def normalize(self):
         quatNormalized = Quaternion()
-        quatNorm = self.norm()
-        quatNormalized.sca = self.sca / quatNorm
-        quatNormalized.vec = self.vec / quatNorm
+        if not self.isIdentity():
+            quatNorm = self.norm()
+            quatNormalized.sca = self.sca / quatNorm
+            quatNormalized.vec = self.vec / quatNorm
+        else:
+            # Identity quaternion
+            quatNormalized = self
         return quatNormalized
 
     def conjugate(self):
         conjQuat = Quaternion()
+        conjQuat.sca = self.sca
         conjQuat.vec = -self.vec
         return conjQuat
     
@@ -46,32 +57,48 @@ class Quaternion:
         qw = self.sca
         qx = self.vec[0]
         qy = self.vec[1]
-        qz = self.vec[2]
-        sca = self.sca
-        vec = self.vec        
+        qz = self.vec[2]     
         phi   = m.atan2(2 * (qw*qx + qy*qz), (1 - 2*(qx**2+qy**2)))
         theta = m.asin(2 * (qw*qy - qz*qx)) 
         psi   = m.atan2(2 * (qw*qz + qx*qy), (1 - 2*(qy**2+qz**2)))
+        # Handle gimbal lock
+        if (abs(theta-pi/2) < 1e-10):
+            phi = 0
+            psi = -2*m.atan2(qx, qw)
+        elif (abs(theta+pi/2) < 1e-10):
+            phi = 0
+            psi = 2*m.atan2(qx, qw)
+        
         eulerAngles = np.array([phi, theta, psi])
         return eulerAngles
 
     def toDcm(self):
         # Source: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        # Source: https://www.vectornav.com/resources/inertial-navigation-primer/math-fundamentals/math-attitudetran
         qw = self.sca
         qx = self.vec[0]
         qy = self.vec[1]
         qz = self.vec[2]
         M11 = qw**2 + qx**2 - qy**2 - qz**2
-        M12 = 2*(qx*qy - qw*qz)
-        M13 = 2*(qw*qy + qw*qz)
-        M21 = 2*(qx*qy + qw*qz)
+        M12 = 2*(qx*qy + qz*qw)
+        M13 = 2*(qx*qz - qy*qw)
+        M21 = 2*(qx*qy - qz*qw)
         M22 = qw**2 - qx**2 + qy**2 - qz**2
-        M23 = 2*(qy*qz - qw*qx)
-        M31 = 2*(qx*qz - qw*qy)
-        M32 = 2*(qw*qx + qy*qz)
+        M23 = 2*(qy*qz + qx*qw)
+        M31 = 2*(qx*qz + qy*qw)
+        M32 = 2*(qy*qz - qx*qw)
         M33 = qw**2 - qx**2 - qy**2 + qz**2
         dcm = np.array([[M11, M12, M13], [M21, M22, M23], [M31, M32, M33]])
         return dcm
+
+    def toVecRot(self):
+        if not self.isIdentity():
+            vec = self.vec/np.linalg.norm(self.vec)
+            ang = 2*np.arccos(self.sca)
+        else:
+            vec = np.array([1, 0, 0])
+            ang = 0
+        return (vec, ang)
 
     def propagateState(self, angleRates_B, simParam):
         xPrev = self.toVec()
@@ -86,7 +113,7 @@ class Quaternion:
         k4 = quatDot(trans_VecToQuat(x4, True), angleRates_B).toVec()
         xNext = xPrev + Ts/6 * (k1 + 2*k2 + 2*k3 + k4)
         
-        quatNext = trans_VecToQuat(xNext, True)
+        quatNext = trans_VecToQuat(xNext, True).normalize()
         self.sca = quatNext.sca
         self.vec = quatNext.vec
      
@@ -108,9 +135,9 @@ def multiplyQuat(quat1, quat2):
 
 
 # Quaternion rotation
-def applyRotation(qAB, v_B):
+def applyRotation(qBA, v_B):
     quat_v_B = Quaternion(0, v_B)
-    quat_v_A = multiplyQuat(multiplyQuat(qAB, quat_v_B), qAB.conjugate())
+    quat_v_A = multiplyQuat(multiplyQuat(qBA, quat_v_B), qBA.conjugate())
     return quat_v_A.vec
 
 
@@ -179,7 +206,72 @@ def trans_EulerAngToQuat(eulerAngles):
     quat.vec[0] = sr*cp*cy - cr*sp*sy
     quat.vec[1] = cr*sp*cy + sr*cp*sy
     quat.vec[2] = cr*cp*sy - sr*sp*cy
+
     return quat
+
+# DCM to 321 euler angles
+def trans_DcmToEulerAng(dcm):
+    M11 = dcm[0, 0]
+    M12 = dcm[0, 1]
+    M13 = dcm[0, 2]
+    M21 = dcm[1, 0]
+    M22 = dcm[1, 1]
+    M23 = dcm[1, 2]
+    M31 = dcm[2, 0]
+    M32 = dcm[2, 1]
+    M33 = dcm[2, 2]
+
+    phi = m.atan2(M12, M11)
+    theta = -m.asin(M13)
+    psi = m.atan2(M23, M33)
+    eulerAngles = np.array([phi, theta, psi])
+
+    return eulerAngles
+
+# DCM to quaternion
+def trans_DcmToQuat(dcm):
+    # Source: https://www.vectornav.com/resources/inertial-navigation-primer/math-fundamentals/math-attitudetran
+    quat = Quaternion()
+    M11 = dcm[0, 0]
+    M12 = dcm[0, 1]
+    M13 = dcm[0, 2]
+    M21 = dcm[1, 0]
+    M22 = dcm[1, 1]
+    M23 = dcm[1, 2]
+    M31 = dcm[2, 0]
+    M32 = dcm[2, 1]
+    M33 = dcm[2, 2]
+
+    qw2 = 1/4 * (1 + M11 + M22 + M33)
+    qx2 = 1/4 * (1 + M11 - M22 - M33)
+    qy2 = 1/4 * (1 - M11 + M22 - M33)
+    qz2 = 1/4 * (1 - M11 - M22 + M33)
+
+    idxMax = np.argmax([qx2, qy2, qz2, qw2])
+
+    if (idxMax == 0):
+        quat.vec = 1/(4*np.sqrt(qx2)) * np.array([4*qx2, M12+M21, M31+M13])
+        quat.sca = 1/(4*np.sqrt(qx2)) * (M23 - M32)
+
+    elif (idxMax == 1):
+        quat.vec = 1/(4*np.sqrt(qy2)) *  np.array([M12+M21, 4*qy2, M23+M32])
+        quat.sca = 1/(4*np.sqrt(qy2)) * (M31 - M13)  
+
+    elif (idxMax == 2):
+        quat.vec = 1/(4*np.sqrt(qz2)) * np.array([M31+M13, M23+M32, 4*qz2])
+        quat.sca = 1/(4*np.sqrt(qz2)) * (M12 - M21)
+
+    else:
+        quat.vec = 1/(4*np.sqrt(qw2)) * np.array([M23-M32, M31-M13, M12-M21])
+        quat.sca = np.sqrt(qw2)
+
+    
+    return quat
+
+# 321 euler angles to DCM
+def trans_EulerAngToDcm(eulerAngles):
+    dcm = Rx(eulerAngles[0]) * Ry(eulerAngles[1]) * Rz(eulerAngles[2])
+    return dcm
 
 
 # X axis rotation DCM

src/fsw/fswControl.py
@@ -6,6 +6,7 @@ Created on Sun Jun 30 22:06:57 2024
 """
 
 import numpy as np
+import src.attitudeKinematics as attitudeKinematics
 
 
 # --------------------------------------------------
@@ -18,8 +19,10 @@ class FswControlParam:
         self.rateDampingKd = 10.0*np.eye(3)
         self.attControlKp = 0.1*np.eye(3)
         self.attControlKd = 10*np.eye(3)
+        self.inertiaSc_B = np.identity(3) # Inertia at the center of mass, in body frame [kg*m^2]
+        self.swInertiaTrqCompensation = False
 
-    def attControlSimplifiedTuning(self, overshoot, timeResp, inertia):
+    def getAttCtrlTuningFrom_overshootAndTimeResp(self, overshoot, timeResp, inertia):
         # Get damping
         damp = np.sqrt(np.log(overshoot)**2/(np.pi**2 + np.log(overshoot)**2))
         # Get natural pulse
@@ -28,9 +31,21 @@ class FswControlParam:
         # Get controller gains
         Kp = omega_n**2 * inertia
         Kd = 2*damp*omega_n * inertia
+        
+        print("damping: %.4f [-]" % (damp))
+        print("natural frequency: %.4f [rad/s]" % (omega_n))
 
         self.attControlKd = Kd
         self.attControlKp = Kp
+
+    def getAttCtrlTuningFrom_dampingAndNaturalFrequency(self, damp, omega_n, inertia):
+        # Get controller gains
+        Kp = omega_n**2 * inertia
+        Kd = 2*damp*omega_n * inertia
+
+        self.attControlKd = Kd
+        self.attControlKp = Kp
+
 
 # --------------------------------------------------
 # FUNCTIONS
@@ -41,13 +56,32 @@ def thrustControl(fswControlParam):
     return forceCtrl_B
 
 def rateControl(fswControlParam, angRateEst_B, angRateGuid_B):
-    torqueCtrl_B = np.matmul(fswControlParam.rateDampingKd, (angRateGuid_B - angRateEst_B))
-    
+    torqueCtrl_B = np.matmul(fswControlParam.rateDampingKd, (angRateGuid_B - angRateEst_B)) 
     return torqueCtrl_B
 
+def rateControlNew(fswControlParam, angRateEst_BR_B):
+    torqueCtrl_B = -np.matmul(fswControlParam.rateDampingKd, angRateEst_BR_B) 
+    return torqueCtrl_B    
+
 def attitudeControl(fswControlParam, angRateMeas_B, eulerAngMeas_BI, angRateGuid_B, eulerAngGuid_BI):
-    torqueCtrl_B = np.matmul(fswControlParam.attControlKd, (angRateGuid_B - angRateMeas_B)) + np.matmul(fswControlParam.attControlKp, (eulerAngGuid_BI - eulerAngMeas_BI))
-    
+    torqueCtrl_B = np.matmul(fswControlParam.attControlKd, (angRateGuid_B - angRateMeas_B)) - np.matmul(fswControlParam.attControlKp, (eulerAngGuid_BI - eulerAngMeas_BI))
+    return torqueCtrl_B
+
+def attitudeControlNew(fswControlParam, angRateEst_BR_B, qEstBR):
+    # 1. Using rotation angle + vector
+    # (rotDirEst_B, rotAngEst) = qEstBR.toVecRot()
+    # torqueCtrl_B = -np.matmul(fswControlParam.attControlKd, angRateEst_BR_B) - np.matmul(fswControlParam.attControlKp, rotDirEst_B*rotAngEst)
+    # 2. Using rotation angle + vector (alternative)
+    # if (rotAngEst > np.pi):
+    #     torqueCtrl_P_B = + np.matmul(fswControlParam.attControlKp, rotDirEst_B*(2*np.pi-rotAngEst))
+    # else:
+    #     torqueCtrl_P_B = - np.matmul(fswControlParam.attControlKp, rotDirEst_B*rotAngEst)
+    # torqueCtrl_D_B = -np.matmul(fswControlParam.attControlKd, angRateEst_BR_B)
+    # torqueCtrl_B = torqueCtrl_D_B + torqueCtrl_P_B
+    # 3. Using euler angles
+    eulerAngEst_BR = qEstBR.toEuler()
+    #torqueCtrl_B = -np.matmul(fswControlParam.attControlKd, angRateEst_BR_B) - np.matmul(fswControlParam.attControlKp, qEstBR.vec)
+    torqueCtrl_B = -np.matmul(fswControlParam.attControlKd, angRateEst_BR_B) - np.matmul(fswControlParam.attControlKp, eulerAngEst_BR)
     return torqueCtrl_B
 
 def offControl() :
@@ -55,17 +89,37 @@ def offControl() :
     torqueCtrl_B = np.array([0, 0, 0])
     return (forceCtrl_B, torqueCtrl_B)
 
+
+def inertiaTrqCompensation(fswControlParam, angRateEst_BI_B):
+    inertiaSc_B = fswControlParam.inertiaSc_B
+    trq_B = - np.cross(angRateEst_BI_B, np.matmul(inertiaSc_B, angRateEst_BI_B))
+    trq_B = np.array([0, 0, 0])
+    return trq_B
+
 def computeControl(fswControlParam, fswBus):
+    # Initialize output bus
+    fswControlBusOut = fswBus.subBuses["control"]
+
     # Retrieve useful inputs
-    angRateEst_B =  fswBus.subBuses["estimation"].signals["angRateEst_BI_B"].value
-    eulerAngEst_BI =  fswBus.subBuses["estimation"].signals["eulerAngEst_BI"].value
-    angRateGuid_B = fswBus.subBuses["guidance"].signals["angRateGuid_BI_B"].value
-    eulerAngGuid_BI = fswBus.subBuses["guidance"].signals["eulerAngGuid_BI"].value
+    angRate_RI_R = fswBus.subBuses["guidance"].signals["angRate_RI_R"].value
+    qRI_sca = fswBus.subBuses["guidance"].signals["qRI_sca"].value
+    qRI_vec = fswBus.subBuses["guidance"].signals["qRI_vec"].value
+
+    angRateEst_BI_B =  fswBus.subBuses["estimation"].signals["angRateEst_BI_B"].value
+    qBIest_sca =  fswBus.subBuses["estimation"].signals["qBIest_sca"].value
+    qBIest_vec =  fswBus.subBuses["estimation"].signals["qBIest_vec"].value
+
     aocsCtrMode = fswBus.subBuses["modeMgt"].signals["aocsCtrMode"].value
     aocsCtrActMode = fswBus.subBuses["modeMgt"].signals["aocsCtrActMode"].value
 
-    # Initialize output bus
-    fswControlBusOut = fswBus.subBuses["control"]
+    # Compute the spacecraft attitude and angular rates with respect to the guidance reference frame
+    qBIest = attitudeKinematics.Quaternion(qBIest_sca, qBIest_vec)
+    qRI = attitudeKinematics.Quaternion(qRI_sca, qRI_vec)
+    qEstBR = attitudeKinematics.multiplyQuat(qRI.conjugate(), qBIest).normalize()
+
+    angRateEst_BR_B = angRateEst_BI_B - np.matmul(qEstBR.toDcm(), angRate_RI_R)
+    eulerAngEst_BR = qEstBR.toEuler()
+    (rotDirEst_BR_B, rotAngEst_BR) = qEstBR.toVecRot()
 
     # Compute control toraque depending on the current control mode
     if (aocsCtrMode == "CTRLMODE_OFF"):
@@ -73,18 +127,23 @@ def computeControl(fswControlParam, fswBus):
         (forceCtrl_B, torqueCtrl_B) = offControl()
     elif (aocsCtrMode == "CTRLMODE_RATE_DAMP_CTRL"):
         # Rate damping control mode
-        torqueCtrl_B = rateControl(fswControlParam, angRateEst_B, angRateGuid_B)
+        # torqueCtrl_B = rateControl(fswControlParam, angRateEst_BR_B)
+        torqueCtrl_B = rateControlNew(fswControlParam, angRateEst_BR_B)
         forceCtrl_B = np.array([0, 0, 0])
     elif (aocsCtrMode == "CTRLMODE_ATT_CTRL"):
         # Inertial attitude control mode
-        torqueCtrl_B = attitudeControl(fswControlParam, angRateEst_B, eulerAngEst_BI, angRateGuid_B, eulerAngGuid_BI)
+        torqueCtrl_B = attitudeControlNew(fswControlParam, angRateEst_BR_B, qEstBR)
         forceCtrl_B = np.array([0, 0, 0])
     elif (aocsCtrMode == "CTRLMOD_THRUST_CTRL"):
-        torqueCtrl_B = attitudeControl(fswControlParam, angRateEst_B, eulerAngEst_BI, angRateGuid_B, eulerAngGuid_BI)
+        torqueCtrl_B = attitudeControlNew(fswControlParam, angRateEst_BR_B, qEstBR)
         forceCtrl_B = np.array([0, 0, 0])
     else:
         # OFF control mode or current mode is not defined
         (forceCtrl_B, torqueCtrl_B) = offControl()
+
+    # Apply feed-forward inertia torque compensation
+    if fswControlParam.swInertiaTrqCompensation:
+        torqueCtrl_B = torqueCtrl_B + inertiaTrqCompensation(fswControlParam, angRateEst_BI_B)        
 
     
     # Switch between THR and RW control torques
@@ -110,6 +169,11 @@ def computeControl(fswControlParam, fswBus):
     fswControlBusOut.signals["forceCtrlThr_B"].update(forceCtrlThr_B)
     fswControlBusOut.signals["torqueCtrlThr_B"].update(torqueCtrlThr_B)
     fswControlBusOut.signals["torqueCtrlRw_B"].update(torqueCtrlRw_B)
+    fswControlBusOut.signals["angRateEst_BR_B"].update(angRateEst_BR_B)
+    fswControlBusOut.signals["eulerAngEst_BR"].update(eulerAngEst_BR)
+    fswControlBusOut.signals["rotAngEst_BR"].update(rotAngEst_BR)
+    fswControlBusOut.signals["qEstBR_sca"].update(qEstBR.sca)
+    fswControlBusOut.signals["qEstBR_vec"].update(qEstBR.vec)
         
     return (fswControlBusOut)
 

src/fsw/fswGuidance.py
@@ -6,9 +6,11 @@ Created on Sun Jun 30 22:06:57 2024
 """
 
 import numpy as np
+import src.attitudeKinematics as attitudeKinematics
 
-deg2rad = np.pi/180
-
+# To be moved as common constants
+pi  = np.pi
+deg2rad = pi/180
 
 # --------------------------------------------------
 # CLASSES
@@ -17,7 +19,7 @@ deg2rad = np.pi/180
 class FswGuidanceParam:
     # To be moved as higher level class
     def __init__(self):
-        self.GUIDMODE_ATT_INERT_eulerAngGuid_BI = np.array([0, 0, 0])*deg2rad
+        self.GUIDMODE_ATT_INERT_eulerAngGuid_RI = np.array([0, 0, 0])*deg2rad
         self.orbitRate = 0 # [rad/s]
 
 
@@ -26,55 +28,65 @@ class FswGuidanceParam:
 # --------------------------------------------------
 
 def rateDampingGuidance():
-    angRateGuid_B = np.array([0, 0, 0])
-    eulerAngGuid_BI = np.array([0, 0, 0])
-    return (angRateGuid_B, eulerAngGuid_BI)
+    angRate_RI_R = np.array([0, 0, 0])
+    qRI = attitudeKinematics.Quaternion()
+    return (angRate_RI_R, qRI)
 
 def attitudeInertialGuidance(fswGuidanceParam):
-    angRateGuid_B = np.array([0, 0, 0])
-    eulerAngGuid_BI = fswGuidanceParam.GUIDMODE_ATT_INERT_eulerAngGuid_BI
-    
-    return (angRateGuid_B, eulerAngGuid_BI)
+    angRate_RI_R = np.array([0, 0, 0])
+    qRI = attitudeKinematics.trans_EulerAngToQuat(fswGuidanceParam.GUIDMODE_ATT_INERT_eulerAngGuid_RI)
+    return (angRate_RI_R, qRI)
 
 def offGuidance():
-    angRateGuid_B = np.array([0, 0, 0])
-    eulerAngGuid_BI = np.array([0, 0, 0])
-    
-    return (angRateGuid_B, eulerAngGuid_BI)
+    angRate_RI_R = np.array([0, 0, 0])
+    qRI = attitudeKinematics.Quaternion()
+    return (angRate_RI_R, qRI)
 
-def nadirGuidance(fswGuidanceParam, eulerAng_LI):
-    angRateGuid_B = np.array([0, -fswGuidanceParam.orbitRate, 0])
-    eulerAngGuid_BI = eulerAng_LI
-    
-    return (angRateGuid_B, eulerAngGuid_BI)
+def nadirGuidance(fswGuidanceParam, angRate_LI_L, qLI):
+    angRate_RI_R = angRate_LI_L
+    qRI = qLI
+    return (angRate_RI_R, qRI)
 
-def computeGuidance(fswGuidanceParam, lvlhFrame, modelsBus, fswBus):
-    # Retrieve useful inputs
-    zL_B = modelsBus.subBuses["dynamics"].subBuses["frames"].signals["zL_B"].value
-    zL_I = modelsBus.subBuses["dynamics"].subBuses["frames"].signals["zL_I"].value
-    aocsGuidMode = fswBus.subBuses["modeMgt"].signals["aocsGuidMode"].value
-    
+def computeGuidance(fswGuidanceParam, envBusIn, fswBus):
     # Initialize output bus
     fswGuidBusOut = fswBus.subBuses["guidance"]
+
+    # Retrieve signals / parameters
+    aocsGuidMode = fswBus.subBuses["modeMgt"].signals["aocsGuidMode"].value
+    angRate_LI_L = envBusIn.signals["angRate_LI_L"].value
+    qLI_sca = envBusIn.signals["qLI_sca"].value
+    qLI_vec = envBusIn.signals["qLI_vec"].value
+
+    # Reconstruct the LVLH frame quaternion
+    qLI = attitudeKinematics.Quaternion(qLI_sca, qLI_vec)
 
     # Compute guidance angular rates and euler angles depending on the current guidance mode
     if (aocsGuidMode == "GUIDMODE_ATT_INERT"):
         # Inertial attitude guidance mode
-        (angRateGuid_B, eulerAngGuid_BI) = attitudeInertialGuidance(fswGuidanceParam)
+        (angRate_RI_R, qRI) = attitudeInertialGuidance(fswGuidanceParam)
     elif (aocsGuidMode == "GUIDMODE_RATE_DAMPING"):
         # Rate damping guidance mode
-        (angRateGuid_B, eulerAngGuid_BI) = rateDampingGuidance()
+        (angRate_RI_R, qRI) = rateDampingGuidance()
     elif (aocsGuidMode == "GUIDMODE_ATT_NADIR"):
         # Nadir guidance mode
-        (angRateGuid_B, eulerAngGuid_BI) = nadirGuidance(fswGuidanceParam, lvlhFrame.eulerAng_LI)
+        (angRate_RI_R, qRI) = nadirGuidance(fswGuidanceParam, angRate_LI_L, qLI)
     else:
         # OFF guidance mode or current mode is not defined
         aocsGuidMode = "GUIDMODE_OFF"
-        (angRateGuid_B, eulerAngGuid_BI) = offGuidance()   
+        (angRate_RI_R, qRI) = offGuidance()   
     
-    # Update output bus signals
-    fswGuidBusOut.signals["angRateGuid_BI_B"].update(angRateGuid_B)
-    fswGuidBusOut.signals["eulerAngGuid_BI"].update(eulerAngGuid_BI)
+    # Compute guidance with respect to the nadir reference frame
+    qRL = attitudeKinematics.multiplyQuat(qRI, qLI.conjugate())
+
+    # Compute euler angles
+    eulerAng_RI = qRI.toEuler() 
+    eulerAng_RL = qRL.toEuler()
+
+    fswGuidBusOut.signals["eulerAng_RI"].update(eulerAng_RI)
+    fswGuidBusOut.signals["eulerAng_RL"].update(eulerAng_RL)
+    fswGuidBusOut.signals["qRI_sca"].update(qRI.sca)
+    fswGuidBusOut.signals["qRI_vec"].update(qRI.vec)
+    fswGuidBusOut.signals["angRate_RI_R"].update(angRate_RI_R)
 
     return fswGuidBusOut 
 
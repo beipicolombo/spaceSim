76e4d3564aa784347ea1e312da49f514f9594973
beipicolombo <beipicolombo@protonmail.com>
2025-03-23 12:23:18 +0100

    051 MOD to add function to initialize dynamics (angular rates and attitude)

--- src/attitudeDynamics.py
+++ src/attitudeDynamics.py
@@ -19,29 +19,68 @@ deg2rad = pi/180
 
 # Attitude dynamics state
 class DynamicsState():
-    def __init__(self):
-        # Angular rates between S/C and inertial frame in S/C frame
-        angRate_BI_B = np.zeros(3)
-        # Angular rates between S/c and LVLV frame in S/C frame
-        angRate_BL_B = np.zeros(3)
-        # Attitude quaternion between S/C frame and inetial frame
-        qBI = attitudeKinematics.Quaternion(1, np.array([0, 0, 0]))
-        qBI = qBI.normalize()
-
+    def __init__(self, qBI, angRate_BI_B):
         self.angRate_BI_B = angRate_BI_B
-        self.angRate_BL_B = angRate_BL_B
-        self.qBI = qBI 
+        self.qBI = qBI
             
     def propagateState(self, attDynParam, simParam, modelsBus):
+        # Initialize output bus
+        modelsBusOut = modelsBus
         # Retrieve useful inputs
         torqueTot_B = modelsBus.subBuses["dynamics"].subBuses["attitude"].signals["torqueTot_B"].value
         # Propagate state
         self.qBI.propagateState(self.angRate_BI_B, simParam)
         self.angRate_BI_B = propagateAngRates(self.angRate_BI_B, torqueTot_B, attDynParam, simParam)
+        # Set output bus signals
+        modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["qBI_sca"].update(self.qBI.sca)
+        modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["qBI_vec"].update(self.qBI.vec)
+        modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["angRate_BI_B"].update(self.angRate_BI_B)
+        modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].update(self.qBI.toEuler())
+        
+        return (self, modelsBusOut)
     
 # --------------------------------------------------
 # FUNCTIONS
 # --------------------------------------------------
+def setInitialDynStateAndBus(modelsBus, simParam, qInitVec = np.array([1, 0, 0, 0]), angRateInit_B = np.zeros(3)):
+    # Initialize output bus
+    modelsBusOut = modelsBus
+
+    # Retrieve inputs
+    swInitAttitudeFrame = simParam.simOptions.swInitAttitudeFrame
+    qInit = attitudeKinematics.trans_VecToQuat(qInitVec, isScalarFirst = True)
+
+    if (swInitAttitudeFrame == "L"):
+        # Initial attitude is initialized wrt nadir reference frame
+        qLI_sca = modelsBus.subBuses["environment"].signals["qLI_sca"].value 
+        qLI_vec = modelsBus.subBuses["environment"].signals["qLI_vec"].value
+        qLI = attitudeKinematics.Quaternion(qLI_sca, qLI_vec)
+        angRate_LI_L = modelsBus.subBuses["environment"].signals["angRate_LI_L"].value
+        angRate_BL_B = angRateInit_B
+        qBL = qInit
+        # Initialize attitude and angular rates
+        angRate_BI_B = angRate_BL_B + np.matmul(qBL.toDcm(), angRate_LI_L)
+        qBI = attitudeKinematics.multiplyQuat(qLI, qBL)
+    elif (swInitAttitudeFrame == "J"):
+        # Initial attitude is initialized wrt inertial reference frame
+        angRate_BI_B = angRateInit_B
+        qBI = qInit
+    else:
+        # Initial attitude is initialized wrt inertial reference frame by default
+        angRate_BI_B = np.zeros(3)
+        qBI = attitudeKinematics.Quaternion()
+    qBI = qBI.normalize()    
+
+    # Initialize dynamics state
+    dynState = DynamicsState(qBI, angRate_BI_B)
+
+    # Update output bus signals
+    modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["qBI_sca"].update(dynState.qBI.sca)
+    modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["qBI_vec"].update(dynState.qBI.vec)
+    modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["angRate_BI_B"].update(dynState.angRate_BI_B)
+    modelsBusOut.subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].update(dynState.qBI.toEuler())
+
+    return (dynState, modelsBusOut)
 
 # Angular acceleration
 def angRatesDot(angRates, externalTorque, attDynParam):   

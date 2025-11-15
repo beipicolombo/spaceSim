33e672d337c6392186893e6c0c4704e7bbcde355
beipicolombo <beipicolombo@protonmail.com>
2025-11-15 15:12:58 +0000

    501-2 MOD to implement event display

--- app.py
+++ app.py
@@ -5,38 +5,51 @@ import plotly.graph_objects as go
 import src.data.scenarios.scenarioDefinitions as scenarioDefinitions
 from src.data.scenarios.scenarioDefinitions import AVAILABLE_SCENARIOS
 from src.utils.runSimu import runSimu
+import pandas as pd
 
-st.title("AOCS Simulation")
+st.title("SpaceSim")
 
 choice = st.selectbox("Select scenario", list(AVAILABLE_SCENARIOS.keys()))
 scenarioPatchFcnHdl = AVAILABLE_SCENARIOS[choice]
 # result = fn(10)
 # st.write(result)
 
-
 if st.button("Run simulation"):
 
     # Run simulation
     out = runSimu(scenarioPatchFcnHdl)
 
-    # Initialize figures layout
-    fig1 = go.Figure()
-    fig2 = go.Figure()
-    fig3 = go.Figure()
-    fig4 = go.Figure()
-    col1, col2 = st.columns(2)
-
     # Get signals to display
-    time = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].timeseries.timeVec
+    time = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].timeseries.toMin().timeVec
     angles = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].timeseries.rad2deg().dataVec
     angRate = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["angRate_BI_B"].timeseries.rad2deg().dataVec
     angRateNorm = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["angRate_BI_B"].timeseries.getNorm().dataVec
     scAngMom = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["angMomSc_B"].timeseries.dataVec
     scAngMomNorm = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["angMomSc_B"].timeseries.getNorm().dataVec
     isAocsModeTrans = out["fswBus"].subBuses["modeMgt"].signals["isAocsModeTrans"].timeseries.dataVec.squeeze()
+    # Get data to display
+    allEvtDataframe = out["allEvtDataframe"]
 
-    # Update figures
+    # Initialize columns
+    col1, col2 = st.columns(2)
+
+    # Events
+    # ==================================
+    # Write events summary
     with col1:
+        st.write("EVENTS")
+        st.dataframe(allEvtDataframe)
+
+    # Temporal data
+    # ==================================
+    with col2:
+        # Initialize figures layout
+        fig1 = go.Figure()
+        fig2 = go.Figure()
+        fig3 = go.Figure()
+        fig4 = go.Figure()
+    
+        # Update figures
         fig1.add_trace(go.Scatter(x=time, y=angRate[:,0], name="x"))
         fig1.add_trace(go.Scatter(x=time, y=angRate[:,1], name="y"))
         fig1.add_trace(go.Scatter(x=time, y=angRate[:,2], name="z"))
@@ -55,7 +68,6 @@ if st.button("Run simulation"):
                            yaxis_title = "[Nms]")
         st.plotly_chart(fig2, use_container_width=True)
 
-    with col2:
         fig3.add_trace(go.Scatter(x=time, y=angles[:,0], name="roll"))
         fig3.add_trace(go.Scatter(x=time, y=angles[:,1], name="pitch"))
         fig3.add_trace(go.Scatter(x=time, y=angles[:,2], name="yaw"))

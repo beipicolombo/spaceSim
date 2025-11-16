7373a87cc3762cc5fe4590809752a756df4e7a1f
beipicolombo <beipicolombo@protonmail.com>
2025-11-16 00:06:09 +0000

    501 MOD to add loading when running the simulation, use containers for organization

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-import streamlit as st
 import numpy as np
+import time
+
+import streamlit as st
 import plotly.graph_objects as go
+import pandas as pd
 
 import src.data.scenarios.scenarioDefinitions as scenarioDefinitions
 from src.data.scenarios.scenarioDefinitions import AVAILABLE_SCENARIOS
 from src.utils.runSimu import runSimu
-import pandas as pd
 
 st.title("SpaceSim")
 
-choice = st.selectbox("Select scenario", list(AVAILABLE_SCENARIOS.keys()))
+# Run scenario button
+# choice = st.selectbox("Select scenario", list(AVAILABLE_SCENARIOS.keys()))
+choice = "testDevelopment" # [TBW] Unique choice for now
 scenarioPatchFcnHdl = AVAILABLE_SCENARIOS[choice]
-# result = fn(10)
-# st.write(result)
 
-if st.button("Run simulation"):
+runButton = st.button("Run simulation")
+
+# Setup layout
+col1, col2 = st.columns(2)
+
+tileModeMgt = col1.container(border = True)
+tileModeMgt.title("FSW")
+
+tilePlots = col2.container(border = True)
+tilePlots.title("Models")
+
+idxFig = 0
+allFigDict = {}
+
+def addFigure(allFigDict, name):
+    newFig = go.Figure()
+    allFigDict.update({name: newFig})
+    return (newFig, allFigDict)
+
+if runButton:
 
     # Run simulation
-    out = runSimu(scenarioPatchFcnHdl)
+    with st.spinner("Running...", show_time=True):
+        # time.sleep(5)
+        out = runSimu(scenarioPatchFcnHdl)
+    st.success("Done!")
 
     # Get signals to display
     time = out["modelsBus"].subBuses["dynamics"].subBuses["attitude"].signals["eulerAng_BI"].timeseries.toMin().timeVec
@@ -31,65 +55,60 @@ if st.button("Run simulation"):
     # Get data to display
     allEvtDataframe = out["allEvtDataframe"]
 
-    # Initialize columns
-    col1, col2 = st.columns(2)
 
-    # Events
+    # Mode management
     # ==================================
-    # Write events summary
-    with col1:
+    with tileModeMgt:
+        # Write events summary
         st.write("EVENTS")
         st.dataframe(allEvtDataframe)
 
-        # Temporal of AOCS modes
-        fig5 = go.Figure()
-        fig5.add_trace(go.Scatter(x=time, y=aocsMode))
-        fig5.update_layout(title="AOCS Mode",
+        # AOCS modes summary [TBW]
+        (currFig, allFigDict) = addFigure(allFigDict, "aocsMode")
+        currFig.add_trace(go.Scatter(x=time, y=aocsMode))
+        currFig.update_layout(title="AOCS Mode",
                            xaxis_title = "Time [min]",
                            yaxis_title = "-")
-        st.plotly_chart(fig5, use_container_width=True)
+        st.plotly_chart(currFig, use_container_width=True)
 
-    # Temporal data
-    # ==================================
-    with col2:
-        # Initialize figures layout
-        fig1 = go.Figure()
-        fig2 = go.Figure()
-        fig3 = go.Figure()
-        fig4 = go.Figure()
-    
-        # Update figures
-        fig1.add_trace(go.Scatter(x=time, y=angRate[:,0], name="x"))
-        fig1.add_trace(go.Scatter(x=time, y=angRate[:,1], name="y"))
-        fig1.add_trace(go.Scatter(x=time, y=angRate[:,2], name="z"))
-        fig1.add_trace(go.Scatter(x=time, y=angRateNorm, name="norm"))
-        fig1.update_layout(title="Angular rates",
-                           xaxis_title = "Time [min]",
-                           yaxis_title = "[deg/s]")
-        st.plotly_chart(fig1, use_container_width=True)
-
-        fig2.add_trace(go.Scatter(x=time, y=scAngMom[:,0], name="x"))
-        fig2.add_trace(go.Scatter(x=time, y=scAngMom[:,1], name="y"))
-        fig2.add_trace(go.Scatter(x=time, y=scAngMom[:,2], name="z"))
-        fig2.add_trace(go.Scatter(x=time, y=scAngMomNorm, name="norm"))
-        fig2.update_layout(title="Angular momentum",
-                           xaxis_title = "Time [min]",
-                           yaxis_title = "[Nms]")
-        st.plotly_chart(fig2, use_container_width=True)
-
-        fig3.add_trace(go.Scatter(x=time, y=angles[:,0], name="roll"))
-        fig3.add_trace(go.Scatter(x=time, y=angles[:,1], name="pitch"))
-        fig3.add_trace(go.Scatter(x=time, y=angles[:,2], name="yaw"))
-        fig3.update_layout(title="Attitude",
-                           xaxis_title = "Time [min]",
-                           yaxis_title = "[deg]")
-        st.plotly_chart(fig3, use_container_width=True)
-
-        fig4.add_trace(go.Scatter(x=time, y=isAocsModeTrans, name="isAocsModeTrans"))
-        fig4.update_layout(title="isAocsModeTrans",
+        (currFig, allFigDict) = addFigure(allFigDict, "aocsModeTrans")
+        currFig.add_trace(go.Scatter(x=time, y=isAocsModeTrans))
+        currFig.update_layout(title="AOCS mode transition flag",
                            xaxis_title = "Time [min]",
                            yaxis_title = "[-]")
-        st.plotly_chart(fig4, use_container_width=True)
+        st.plotly_chart(currFig, use_container_width=True)
 
-    # st.write("Mode timeline:")
-    # st.write(mode[:20])  # or plot as categorical
+    # Plots
+    # ==================================
+    with tilePlots:
+        # Angular rates plot
+        (currFig, allFigDict) = addFigure(allFigDict, "angRates")
+        currFig.add_trace(go.Scatter(x=time, y=angRate[:,0], name="x"))
+        currFig.add_trace(go.Scatter(x=time, y=angRate[:,1], name="y"))
+        currFig.add_trace(go.Scatter(x=time, y=angRate[:,2], name="z"))
+        currFig.add_trace(go.Scatter(x=time, y=angRateNorm, name="norm"))
+        currFig.update_layout(title="Angular rates",
+                           xaxis_title = "Time [min]",
+                           yaxis_title = "[deg/s]")
+        st.plotly_chart(currFig, use_container_width=True)
+
+        # Angular momentum plot
+        (currFig, allFigDict) = addFigure(allFigDict, "angMom")
+        currFig.add_trace(go.Scatter(x=time, y=scAngMom[:,0], name="x"))
+        currFig.add_trace(go.Scatter(x=time, y=scAngMom[:,1], name="y"))
+        currFig.add_trace(go.Scatter(x=time, y=scAngMom[:,2], name="z"))
+        currFig.add_trace(go.Scatter(x=time, y=scAngMomNorm, name="norm"))
+        currFig.update_layout(title="Angular momentum",
+                           xaxis_title = "Time [min]",
+                           yaxis_title = "[Nms]")
+        st.plotly_chart(currFig, use_container_width=True)
+
+        # Euler angles attitude plot
+        (currFig, allFigDict) = addFigure(allFigDict, "attitudeEulerAng")
+        currFig.add_trace(go.Scatter(x=time, y=angles[:,0], name="roll"))
+        currFig.add_trace(go.Scatter(x=time, y=angles[:,1], name="pitch"))
+        currFig.add_trace(go.Scatter(x=time, y=angles[:,2], name="yaw"))
+        currFig.update_layout(title="Attitude",
+                           xaxis_title = "Time [min]",
+                           yaxis_title = "[deg]")
+        st.plotly_chart(currFig, use_container_width=True)
